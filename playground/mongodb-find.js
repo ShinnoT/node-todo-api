f880d26c1e0e5a7cc856fc0e3d7da2697a6e4eba
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-31 02:46:24 +0900

    querying with DB: find and count method with mogoDB module

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -15,9 +15,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, dbObject) => {
 // you need this from version @3~
   const myDB = dbObject.db('TodoApp');
 
-  myDB.collection('Todos').find().toArray().then((docs) => {
-    console.log('todos');
-    console.log(JSON.stringify(docs, undefined, 2));
+//find all those with completed: true
+  // myDB.collection('Todos').find({completed: true}).toArray().then((docs) => {
+  //   console.log('todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (error) => {
+  //   console.log('unable to fetch todos', error);
+  // });
+
+//find one with id that equals 5a47a70a66c35437aba0845d
+//however _id is not a string, rather, it is a an object called id
+//so we must make a new id object first with the id string
+  // myDB.collection('Todos').find({_id: new ObjectID('5a47a70a66c35437aba0845d')}).toArray().then((docs) => {
+  //   console.log('todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (error) => {
+  //   console.log('unable to fetch todos', error);
+  // });
+
+//counting the number of documents in collection
+  myDB.collection('Todos').find().count().then((count) => {
+    console.log(`todos count: ${count}`);
   }, (error) => {
     console.log('unable to fetch todos', error);
   });
