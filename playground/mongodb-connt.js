dc6ca447389b18121acd35d767b3a14d8db7b426
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-30 20:52:18 +0900

    notes on object ids and object destructuring

--- playground/mongodb-connt.js
+++ playground/mongodb-connt.js
@@ -1,4 +1,10 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// // manually randomely generating new object _ids:
+// let obj = new ObjectID();
+// console.log(obj);
+// // however it is just easier to let mongodb automatically do this
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, dbObject) => {
   if (error) {
@@ -21,18 +27,33 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, dbObject) => {
   //   }
   // });
 
+
+  //here we didnt specify _id property however you can manually set id as well
   myDB.collection('Users').insertOne({
-    name: 'shinno',
-    age: 25,
-    location: 'Japan'
+    name: 'shinno2',
+    age: 27,
+    location: 'Korea'
   }, (error, result) => {
     if (error) {
       console.log('unable to insert user ', error);
     } else {
-      console.log(JSON.stringify(result.ops, undefined, 2));
+      // console.log(JSON.stringify(result.ops, undefined, 2));
+      // result.ops is an array of all the objects in the collection
+      console.log(result.ops[0]._id.getTimestamp());
     }
   });
 
   dbObject.close();
 });
 
+
+
+//object DESTRUCTURING
+
+// let user = {name: 'shinno', age: 25};
+// // to destrcuture and get a variable of the user name:
+// let {name} = user;
+// // this is the same thing as:
+// // let name = user.name;
+// // you can also get two variables at once by:
+// let {name, age} = user
