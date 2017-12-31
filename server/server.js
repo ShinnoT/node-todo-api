55b6c175a1a649bf42cf64982f673acae41d8de3
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2017-12-31 18:46:53 +0900

    model validations

--- server/server.js
+++ server/server.js
@@ -6,13 +6,22 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 //model validations
 let Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    //even if type is String, if you put boolean or number when
+    //creating new instance, it will still work because it will wrap input in quotes
+    //something to be cautious of
+    type: String,
+    required: true,
+    minLength: 1,
+    //trim will trim trailing and preceeding white spaces
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
