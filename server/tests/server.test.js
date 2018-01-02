e457e6e4a503c5734a57fda28219b78bc3640691
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-01-02 18:52:41 +0900

    todo get request testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,15 +5,27 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 
+todos = [{
+  text: "first test todo"
+}, {
+  text: "second test todo"
+}];
+
+
 //however in our test we assume that there is nothing in the DB
 //most of the time there will actually be data in DB
 //so we must run:
 beforeEach((done) => {
   Todo.remove({}).then(() => {
     // console.log('removed everything from DB before test');
+    // now however we include test todos (seeds)
+    return Todo.insertMany(todos);
+  }).then(() => {
+    // chaining commands here because returning todos up there
     done();
   });
 });
+//this says: before each test remove everything and add the two seeds
 
 describe('POST /todos', () => {
 
@@ -32,7 +44,7 @@ describe('POST /todos', () => {
         if (error) {
           return done(error);
         }
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -57,7 +69,7 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((error) => {
           done(error);
@@ -66,4 +78,18 @@ describe('POST /todos', () => {
   });
 
 
+});
+
+
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((response) => {
+        expect(response.body.todos.length).toBe(2);
+      })
+      .end(done);
+  });
 });
