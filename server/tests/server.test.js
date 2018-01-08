d5fd800800fdda2bfc917d255a71a21204761636
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-01-09 00:58:28 +0900

    users /login tests completed

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -289,3 +289,53 @@ describe('POST /users', () => {
   });
 
 });
+
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((response) => {
+        expect(response.headers['x-auth']).toBeDefined();
+      })
+      .end((error, response) => {
+        if (error) {
+          return done(error);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens[0].access).toBe('auth');
+          expect(user.tokens[0].token).toBe(response.headers['x-auth']);
+          done();
+        }).catch((error) => done(error));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: 'whatever@email.com',
+        password: users[1].password
+      })
+      .expect(400)
+      .expect((response) => {
+        expect(response.headers['x-auth']).toBeUndefined();
+      })
+      .end((error, response) => {
+        if (error) {
+          return done(error);
+        }
+
+        User.findById(users[1]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((error) => done(error));
+      });
+  });
+});
